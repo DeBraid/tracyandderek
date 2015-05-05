aff7abb2a3926cc79aeb6558ec06f491fd9fc140
DeBraid <de.braid@gmail.com>
2015-05-05 10:52:05 -0400

    navbar sub clicks will collapse header

--- app/client/templates/nav/nav.js
+++ app/client/templates/nav/nav.js
@@ -2,18 +2,31 @@
 /* Nav: Event Handlers */
 /*****************************************************************************/
 Template.Nav.events({
+	'click .navbar-header > button': function (event,template) {
+		var menuState = Session.get('menuOpen');
+		return Session.set('menuOpen', !menuState );
+	},
+	'click ul.navbar-nav > a': function () {
+		$('.collapse.in').collapse('hide');
+		return Session.set('menuOpen', !Session.get('menuOpen') );
+		
+	}
 });
 
 /*****************************************************************************/
 /* Nav: Helpers */
 /*****************************************************************************/
 Template.Nav.helpers({
+	isMenuOpen: function () {
+		return Session.get('menuOpen');
+	}
 });
 
 /*****************************************************************************/
 /* Nav: Lifecycle Hooks */
 /*****************************************************************************/
 Template.Nav.created = function () {
+	return Session.set('menuOpen', false);
 };
 
 Template.Nav.rendered = function () {
