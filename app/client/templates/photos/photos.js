6e45e8affd83ad1c6b70c6f33e8ec5a387b0d671
DeBraid <de.braid@gmail.com>
2015-09-05 12:33:18 -0400

    images loading

--- app/client/templates/photos/photos.js
+++ app/client/templates/photos/photos.js
@@ -1,3 +1,48 @@
+
+
+/*****************************************************************************/
+/* Photos: Helpers */
+/*****************************************************************************/
+Template.Photos.helpers({
+  pics: function () {
+    return [
+      { src: 'day-of/bridal-party.JPG' },
+      { src: 'day-of/bridesmaids-tracy-peak.JPG' },
+      { src: 'day-of/derek-lisa.JPG' },
+      { src: 'day-of/roady-popo.JPG' },
+      { src: 'day-of/tracy-concerned.JPG' },
+      { src: 'day-of/tracy-photoshoot.JPG' },
+      { src: 'day-of/wedding-party-2.JPG' },
+      { src: 'day-of/wedding-party-cele.JPG' },
+      { src: 'day-of/bridesmaids-look-at-tracy.JPG', klass: 'portrait-image' },
+      { src: 'day-of/tracy-full-dress.JPG', klass: 'portrait-image-oppo' },
+      { src: 'day-of/brian-emily.JPG', klass: 'portrait-image' },
+      { src: 'day-of/wedding-party.JPG', klass: 'portrait-image' },
+
+    ]
+  },
+  image: function () {
+    return Session.get('img');
+  },
+  newPhoto: function () {
+    return Session.get('isNewPhoto');
+  }
+});
+
+/*****************************************************************************/
+/* Photos: Lifecycle Hooks */
+/*****************************************************************************/
+Template.Photos.created = function () {
+  return Session.set('isNewPhoto', false);
+};
+
+Template.Photos.rendered = function () {
+};
+
+Template.Photos.destroyed = function () {
+  return Session.set('isNewPhoto', false);
+};
+
 /*****************************************************************************/
 /* Photos: Event Handlers */
 /*****************************************************************************/
@@ -20,29 +65,3 @@ Template.Photos.events({
     });
   }
 });
-
-/*****************************************************************************/
-/* Photos: Helpers */
-/*****************************************************************************/
-Template.Photos.helpers({
-  image: function () {
-    return Session.get('img');
-  },
-  newPhoto: function () {
-    return Session.get('isNewPhoto');
-  }
-});
-
-/*****************************************************************************/
-/* Photos: Lifecycle Hooks */
-/*****************************************************************************/
-Template.Photos.created = function () {
-  return Session.set('isNewPhoto', false);
-};
-
-Template.Photos.rendered = function () {
-};
-
-Template.Photos.destroyed = function () {
-  return Session.set('isNewPhoto', false);
-};
