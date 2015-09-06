0de0d8f8aff90b6c0e03faec4eb95f6089330f18
DeBraid <de.braid@gmail.com>
2015-09-06 10:08:44 -0400

    toggle big and small images

--- app/client/templates/photos/photos.js
+++ app/client/templates/photos/photos.js
@@ -1,66 +1,66 @@
-
-
 /*****************************************************************************/
 /* Photos: Helpers */
 /*****************************************************************************/
 Template.Photos.helpers({
-  pics: function () {
-    return [
-      { src: 'day-of/bridal-party.JPG' },
-      { src: 'day-of/bridesmaids-tracy-peak.JPG' },
-      { src: 'day-of/derek-lisa.JPG' },
-      { src: 'day-of/roady-popo.JPG' },
-      { src: 'day-of/tracy-full-dress.JPG'},
-      { src: 'day-of/brian-emily.JPG'},
-      { src: 'day-of/tracy-photoshoot.JPG' },
-      { src: 'day-of/tracy-concerned.JPG' },
-      { src: 'day-of/wedding-party-2.JPG' },
-      { src: 'day-of/wedding-party-cele.JPG' },
-      { src: 'day-of/wedding-party.JPG'},
-      { src: 'day-of/bridesmaids-look-at-tracy.JPG'},
-    ]
-  },
-  image: function () {
-    return Session.get('img');
-  },
-  newPhoto: function () {
-    return Session.get('isNewPhoto');
-  }
+    pics: function () {
+        return [{
+            src: 'day-of/bridal-party.JPG'
+        }, {
+            src: 'day-of/bridesmaids-tracy-peak.JPG'
+        }, {
+            src: 'day-of/derek-lisa.JPG'
+        }, {
+            src: 'day-of/roady-popo.JPG'
+        }, {
+            src: 'day-of/tracy-full-dress.JPG'
+        }, {
+            src: 'day-of/brian-emily.JPG'
+        }, {
+            src: 'day-of/tracy-photoshoot.JPG'
+        }, {
+            src: 'day-of/tracy-concerned.JPG'
+        }, {
+            src: 'day-of/wedding-party-2.JPG'
+        }, {
+            src: 'day-of/wedding-party-cele.JPG'
+        }, {
+            src: 'day-of/wedding-party.JPG'
+        }, {
+            src: 'day-of/bridesmaids-look-at-tracy.JPG'
+        }, ]
+    },
+    enlargedPhotos: function () {
+        return Session.get('isPhotoClicked');
+    },
+    photoContainerWidth: function () {
+        if ( Session.get('isPhotoClicked') ) {
+            return 'col-sm-3';
+        } else {
+            return 'col-sm-12';
+        }
+    }
+});
+
+/*****************************************************************************/
+/* Photos: Event Handlers */
+/*****************************************************************************/
+Template.Photos.events({
+    'click #enlarge-photos': function () {
+        var clickState = Session.get('isPhotoClicked');
+        console.log(clickState);
+        return Session.set('isPhotoClicked', !clickState);    
+    }
 });
 
 /*****************************************************************************/
 /* Photos: Lifecycle Hooks */
 /*****************************************************************************/
 Template.Photos.created = function () {
-  return Session.set('isNewPhoto', false);
+    return Session.set('isPhotoClicked', true);
 };
 
-Template.Photos.rendered = function () {
-};
+Template.Photos.rendered = function () {};
 
 Template.Photos.destroyed = function () {
-  return Session.set('isNewPhoto', false);
+    return Session.set('isPhotoClicked', false);
 };
-
-/*****************************************************************************/
-/* Photos: Event Handlers */
-/*****************************************************************************/
-Template.Photos.events({
-  'click [data-action=takePhoto]': function () {
-    var options = {  
-      width: 350,
-      height: 350,
-      quality: 75
-    }
-    MeteorCamera.getPicture(options, function(err, data) {  
-      if (err) {
-        console.log('error', err);
-      }
-      if (data) {
-        Meteor.call('storeNewPhoto', data, function () {
-          Session.set('isNewPhoto', true);
-        });
-      }
-    });
-  }
-});
